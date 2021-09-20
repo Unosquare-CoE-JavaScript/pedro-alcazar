65eacb39a7bd2abe81336f1d5a65d71fa5243102
Pedro Alcázar <pedro.alcazar@unosquare.com>
2021-09-20 13:31:44 -0500

    Add test for checkbox

--- ReactTesting/react-testing/src/App.js
+++ ReactTesting/react-testing/src/App.js
@@ -7,12 +7,21 @@ import { useState } from 'react';
 function App() {
   const [stateButtonColor, setStateButtonColor] = useState({color: 'red'})
   const newColor = stateButtonColor.color === 'red' ? 'blue' : 'red'
+  const [buttonDisabled, setButtonDisabled] = useState(false)
   const handleChangeColor = () => {
     setStateButtonColor({color: newColor})
   }
+  const handleChangeCheckStatus = (e) => {
+    setButtonDisabled(e)
+  }
   return (
     <div className="App">
-      <button style={{backgroundColor: stateButtonColor.color}} onClick={handleChangeColor} >Change to {newColor}</button>
+      <button 
+        style={{backgroundColor: stateButtonColor.color}} 
+        onClick={handleChangeColor} 
+        disabled={buttonDisabled}
+        >Change to {newColor}</button> <br/>
+      <input type="checkbox" onChange={(e) =>{handleChangeCheckStatus(e.target.checked)}}  />
     </div>
   );
 }
--- ReactTesting/react-testing/src/App.test.js
+++ ReactTesting/react-testing/src/App.test.js
@@ -18,3 +18,30 @@ test('button turns blue when clicked',() =>{
   expect(likeElement.textContent).toBe('Change to red')
 });
 
+test('initial conditions', () =>{
+  render (<App />);
+
+  // check that the botton starts out enabled
+  const colorButton  = screen.getByRole('button', {name: 'Change to blue'})
+  expect(colorButton).toBeEnabled();
+
+  // chect that checkbox starts out unchecked
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
+});
+
+test('Checkbox disables button on first click and enables on second click',() =>{
+  render (<App />)
+
+  //Selecting elements
+  const checkbox = screen.getByRole('checkbox');
+  const button = screen.getByRole('button');
+
+  fireEvent.click(checkbox);
+  expect(button).toBeDisabled;
+
+  fireEvent.click(checkbox);
+  expect(button).toBeEnabled;
+})
+
+
