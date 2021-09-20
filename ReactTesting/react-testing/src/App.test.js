3763ab87aaf211bf1eb7acc29792b5f9c460c48d
Pedro Alcázar <pedro.alcazar@unosquare.com>
2021-09-20 12:38:46 -0500

    test for button color

--- ReactTesting/react-testing/src/App.test.js
+++ ReactTesting/react-testing/src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByRole('link',{name: /learn react/i});
-  expect(linkElement).toBeInTheDocument();
+  // FIND AN ELEMENT WITH A ROLE OF BUTTON AND TEXT OF 'CHANGE TO BLUE'
+  const linkElement = screen.getByRole('button',{name: 'Change to blue'});
+  // EXPECT THE BACKGROUND COLOR TO BE RED
+  expect(linkElement).toHaveStyle({backgroundColor: 'red'});
 });
+
+test('button turns blue when clicked',() =>{
+  render(<App />);
+  const likeElement = screen.getByRole('button', {name: 'Change to blue'})
+  expect(likeElement).toHaveStyle({backgroundColor: 'red'})
+  fireEvent.click(likeElement);
+  expect(likeElement).toHaveStyle({backgroundColor: 'blue'})
+  expect(likeElement.textContent).toBe('Change to red')
+});
+
