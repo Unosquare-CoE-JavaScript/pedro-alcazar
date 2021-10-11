b3570762c450997701ca3d7a4f1341b778086135
Pedro Alcázar <pedro.alcazar@unosquare.com>
2021-10-11 10:36:02 -0500

    Decorators

---
+++ Typescript/Decorators.ts
@@ -0,0 +1,37 @@
+function Logger(){
+    return function(constructor: Function){
+        console.log("logging...")
+        console.log(constructor)
+    }
+}
+
+function Logger2(){
+    return function(constructor: Function){
+        console.log("Access to Decorator")
+        console.log(constructor)
+    }
+}
+
+function WithTemplate (template: string, hookId: string){
+    return function(constructor: Function){
+        const hookEl = document.getElementById(hookId)
+        const p = new Person2()
+        if (hookEl){
+            hookEl.innerHTML = template
+            hookEl.querySelector('h1')!.textContent= p.name
+        }
+    }
+}
+@Logger()
+@Logger2()
+@WithTemplate('<hi>My person Project </h1>', 'app')
+
+class Person2 {
+    name =  'Max';
+    constructor(){
+        console.log("creating object");
+    }
+}
+var person = new Person2();
+
+console.log(person)
