62aa7e31cde89971f8ee7fdca15c0d83197679e2
Pedro Alcázar <pedro.alcazar@unosquare.com>
2021-10-11 20:59:17 -0500

    lodash

--- Typescript/app.ts
+++ Typescript/app.ts
@@ -1,23 +1,3 @@
+import _ from 'lodash';
 
-import { Project } from "./models/project"
- 
-
-namespace App {
-    type Listener <T> = (items: T[]) => void;
-
-    class State<T> {
-        protected listeners: Listener<T>[] = [];
-
-        addListener(listenerFn: Listener <T>){
-            this.listeners.push(listenerFn)
-        }
-    }
-
-    class ProjectState extends State<Project>{
-        private projects: Project[] = [];
-        private static instance: ProjectState
-        private constructor(){
-            super();
-        }
-    }
-}
+console.log(_.shuffle([1,2,3]))
