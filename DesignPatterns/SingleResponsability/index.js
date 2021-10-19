358b7f27ce0e3866866ff493388f5ce6f639a6d3
Pedro Alcázar <pedro.alcazar@unosquare.com>
2021-10-19 18:58:38 -0500

    Single responsability

---
+++ DesignPatterns/SingleResponsability/index.js
@@ -0,0 +1,52 @@
+const fs =require('fs');
+class Journal{
+    constructor(){
+        this.entries = {};
+    }
+
+    addEntry(text){
+        let c= ++Journal.count;
+        let entry =  `${c}: ${text}`;
+        this.entries[c] = entry;
+        return c;
+    } 
+
+    removeEntry(index){
+        delete this.entries[index];
+    }
+
+    toString(){
+        return Object.values(this.entries).join('\n');
+    }
+
+    save(filename){
+        fs.writeFileSync(filename, this.toString())
+    }
+
+    load(filename){
+
+    }
+    
+    loadformUrl(url){
+
+    }
+}
+Journal.count = 0;
+
+class PersistenceManager{
+
+    prerocess(){
+
+    }
+    saveToFile(journal, filename){
+        fs.writeFileSync(filename, journal.toString())
+    }
+}
+let j = new Journal();
+j.addEntry('I cried today');
+j.addEntry('I hate a bug');
+console.log(j.toString())
+
+let p = new PersistenceManager();
+let filename = './text.txt';
+p.saveToFile(j,filename);
